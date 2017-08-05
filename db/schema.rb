7157498917a8600a06ed9297e7b44f57fe1d361b
lbias <306508354@qq.com>
2017-08-05 21:59:40 +0800

    migration reset

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,12 @@
 
 ActiveRecord::Schema.define(version: 20170805194918) do
 
+  create_table "contacts", force: :cascade do |t|
+    t.string   "full_name"
+    t.string   "email"
+    t.integer  "phone_number"
+    t.text     "address"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 end
