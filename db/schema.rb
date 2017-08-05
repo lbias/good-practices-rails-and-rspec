fdfc32597c4f925978d319c0d36b7234c15763fe
lbias <306508354@qq.com>
2017-08-05 21:56:01 +0800

    reset

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,4 @@
 
 ActiveRecord::Schema.define(version: 20170805194918) do
 
-  create_table "contacts", force: :cascade do |t|
-    t.string   "full_name"
-    t.string   "email"
-    t.integer  "phone_number"
-    t.text     "address"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
 end
